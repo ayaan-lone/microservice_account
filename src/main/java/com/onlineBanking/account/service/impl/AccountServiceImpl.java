3aa138c926aedc60e5011af0dc2f821d5566eab0
Aryan Saini <aryan.saini@xebia.com>
2024-07-12 02:48:07 +0530

    basic api added with SSL error

--- src/main/java/com/onlineBanking/account/service/impl/AccountServiceImpl.java
+++ src/main/java/com/onlineBanking/account/service/impl/AccountServiceImpl.java
@@ -1,16 +1,35 @@
 package com.onlineBanking.account.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.client.RestTemplate;
+import com.onlineBanking.account.dao.AccountRepository;
+import com.onlineBanking.account.entity.Account;
+import com.onlineBanking.account.request.CreateCardDto;
 import com.onlineBanking.account.service.AccountService;
 
 @Service
-public class AccountServiceImpl implements AccountService{
+public class AccountServiceImpl implements AccountService {
 
-	@Override
-	public String updateBalance() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Autowired
+    private RestTemplate restTemplate;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public void createAccountWithCard(long userId, String accountType) {
+        // Create account logic
+        Account account = new Account();
+        account.setUserId(userId);
+        account.setAccountType(accountType);
+        account.setBalance(0);
+
+        // Save account
+        accountRepository.save(account);
+        
+        CreateCardDto request = new CreateCardDto(userId, accountType);
+        String cardServiceUrl = "https://localhost:8082/api/v1/create";
+        restTemplate.postForObject(cardServiceUrl, request, Object.class);
+    }
 }
