2a50dc36e6500a4973d1bcfcffa38ebafba77e05
Aryan Saini <aryan.saini@xebia.com>
2024-07-12 12:07:16 +0530

    bugs fixed

--- src/main/java/com/onlineBanking/account/service/impl/AccountServiceImpl.java
+++ src/main/java/com/onlineBanking/account/service/impl/AccountServiceImpl.java
@@ -1,6 +1,8 @@
 package com.onlineBanking.account.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.onlineBanking.account.dao.AccountRepository;
@@ -11,25 +13,26 @@ import com.onlineBanking.account.service.AccountService;
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    @Autowired
-    private RestTemplate restTemplate;
+	@Autowired
+	private RestTemplate restTemplate;
 
-    @Autowired
-    private AccountRepository accountRepository;
+	@Autowired
+	private AccountRepository accountRepository;
 
-    @Override
-    public void createAccountWithCard(long userId, String accountType) {
-        // Create account logic
-        Account account = new Account();
-        account.setUserId(userId);
-        account.setAccountType(accountType);
-        account.setBalance(0);
+	@Override
+	public void createAccountWithCard(long userId, String accountType) {
+		// Create account logic
+		Account account = new Account();
+		account.setUserId(userId);
+		account.setAccountType(accountType);
+		account.setBalance(0);
 
-        // Save account
-        accountRepository.save(account);
-        
-        CreateCardDto request = new CreateCardDto(userId, accountType);
-        String cardServiceUrl = "https://localhost:8082/api/v1/create";
-        restTemplate.postForObject(cardServiceUrl, request, Object.class);
-    }
+		// Save account
+		accountRepository.save(account);
+
+		CreateCardDto request = new CreateCardDto(userId, accountType);
+		HttpEntity<CreateCardDto> httpEntity = new HttpEntity<CreateCardDto>(request);
+		String cardServiceUrl = "http://localhost:8082/api/v1/create";
+		restTemplate.exchange(cardServiceUrl, HttpMethod.POST, httpEntity, Object.class);
+	}
 }
